94214b6f03606b2c9d8e15c3520793fc229a3fe4
amira <amiraahmedlbrahim5@gmail.com>
2025-12-23 16:42:57 +0200

    Fix CORS for OpenShift

--- internal/router/router.go
+++ internal/router/router.go
@@ -14,11 +14,11 @@ func New(auth *handlers.AuthHandler, events *handlers.EventHandler, search *hand
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "X-User-ID"},
 		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
+		AllowCredentials: false,
 		MaxAge:           12 * time.Hour,
 	}))
 
